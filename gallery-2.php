783ec41c33c90b0b1d56d3cfea78d48ac6a7271a
muktiswarup <csjena1999@gmail.com>
2026-06-26 12:37:26 +0530

    'second'

---
+++ gallery-2.php
@@ -0,0 +1,158 @@
+<?php include 'header.php'; ?>
+
+    <!-- Page Title -->
+    <section class="page-title" style="background-image: url(assets/images/main-slider/page-title.jpg);">
+        <div class="auto-container">
+            <div class="text-center">
+                <h1>Gallery Pinterest</h1>
+            </div>
+        </div>
+    </section>
+
+
+     <!-- gallery section four -->
+     <section class="gallery-section-four light-bg mx-60 border-shape-top">
+        <div class="auto-container">
+            <!--Filter-->
+            <div class="filters">
+                <ul class="filter-tabs filter-btns">
+                    <li class="filter active" data-role="button" data-filter=".all">All</li>
+                    <li class="filter" data-role="button" data-filter=".cat-1">Spa</li>
+                    <li class="filter" data-role="button" data-filter=".cat-2">Rooms</li>
+                    <li class="filter" data-role="button" data-filter=".cat-3">Activities</li>
+                    <li class="filter" data-role="button" data-filter=".cat-4">Restaurant</li>
+                    <li class="filter" data-role="button" data-filter=".cat-5">Pool</li>
+                </ul>
+            </div>
+                
+            <!--Sortable Galery-->
+            <div class="sortable-masonry">
+                
+                <div class="items-container row">
+                    <!-- Project Block -->
+                    <div class="gallery-block-four gallery-overlay masonry-item all cat-1 col-lg-4 col-md-6">
+                        <div class="inner-box">
+                            <div class="image">
+                                <img src="assets/images/gallery/1.jpg" alt="">
+                            </div>
+                            <div class="overlay-box">
+                                <div class="overlay-inner">
+                                    <div class="content">
+                                        <a href="assets/images/resource/popup-image.jpg" class="lightbox-image link" data-fancybox="gallery"><span class="icon fas fa-eye"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Project Block -->
+                    <div class="gallery-block-four gallery-overlay masonry-item all cat-2 col-lg-4 col-md-6">
+                        <div class="inner-box">
+                            <div class="image">
+                                <img src="assets/images/gallery/2.jpg" alt="">
+                            </div>
+                            <div class="overlay-box">
+                                <div class="overlay-inner">
+                                    <div class="content">
+                                        <a href="assets/images/resource/popup-image.jpg" class="lightbox-image link" data-fancybox="gallery"><span class="icon fas fa-eye"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Project Block -->
+                    <div class="gallery-block-four gallery-overlay masonry-item all cat-3 col-lg-4 col-md-6">
+                        <div class="inner-box">
+                            <div class="image">
+                                <img src="assets/images/gallery/3.jpg" alt="">
+                            </div>
+                            <div class="overlay-box">
+                                <div class="overlay-inner">
+                                    <div class="content">
+                                        <a href="assets/images/resource/popup-image.jpg" class="lightbox-image link" data-fancybox="gallery"><span class="icon fas fa-eye"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Project Block -->
+                    <div class="gallery-block-four gallery-overlay masonry-item all cat-4 col-lg-4 col-md-6">
+                        <div class="inner-box">
+                            <div class="image">
+                                <img src="assets/images/gallery/4.jpg" alt="">
+                            </div>
+                            <div class="overlay-box">
+                                <div class="overlay-inner">
+                                    <div class="content">
+                                        <a href="assets/images/resource/popup-image.jpg" class="lightbox-image link" data-fancybox="gallery"><span class="icon fas fa-eye"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Project Block -->
+                    <div class="gallery-block-four gallery-overlay masonry-item all cat-5 col-lg-4 col-md-6">
+                        <div class="inner-box">
+                            <div class="image">
+                                <img src="assets/images/gallery/5.jpg" alt="">
+                            </div>
+                            <div class="overlay-box">
+                                <div class="overlay-inner">
+                                    <div class="content">
+                                        <a href="assets/images/resource/popup-image.jpg" class="lightbox-image link" data-fancybox="gallery"><span class="icon fas fa-eye"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Project Block -->
+                    <div class="gallery-block-four gallery-overlay masonry-item all cat-2 col-lg-4 col-md-6">
+                        <div class="inner-box">
+                            <div class="image">
+                                <img src="assets/images/gallery/8.jpg" alt="">
+                            </div>
+                            <div class="overlay-box">
+                                <div class="overlay-inner">
+                                    <div class="content">
+                                        <a href="assets/images/resource/popup-image.jpg" class="lightbox-image link" data-fancybox="gallery"><span class="icon fas fa-eye"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Project Block -->
+                    <div class="gallery-block-four gallery-overlay masonry-item all cat-3 col-lg-4 col-md-6">
+                        <div class="inner-box">
+                            <div class="image">
+                                <img src="assets/images/gallery/6.jpg" alt="">
+                            </div>
+                            <div class="overlay-box">
+                                <div class="overlay-inner">
+                                    <div class="content">
+                                        <a href="assets/images/resource/popup-image.jpg" class="lightbox-image link" data-fancybox="gallery"><span class="icon fas fa-eye"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Project Block -->
+                    <div class="gallery-block-four gallery-overlay masonry-item all cat-1 col-lg-4 col-md-6">
+                        <div class="inner-box">
+                            <div class="image">
+                                <img src="assets/images/gallery/7.jpg" alt="">
+                            </div>
+                            <div class="overlay-box">
+                                <div class="overlay-inner">
+                                    <div class="content">
+                                        <a href="assets/images/resource/popup-image.jpg" class="lightbox-image link" data-fancybox="gallery"><span class="icon fas fa-eye"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php include 'footer.php'; ?>
---
+++ room-grid.php
@@ -0,0 +1,144 @@
+<?php include 'header.php'; ?>
+
+    <!-- Page Title -->
+    <section class="page-title" style="background-image: url(assets/images/main-slider/page-title-6.jpg);">
+        <div class="auto-container">
+            <div class="text-center">
+                <h1>Rooms Grid</h1>
+            </div>
+        </div>
+    </section>
+
+
+     <!-- gallery section four -->
+     <section class="room-grid-section pb-3 light-bg mx-60 border-shape-top border-shape-bottom">
+        <div class="auto-container">
+            <div class="title-box text-center">
+                <div class="sub-title mb-4">Service since 1989</div>
+                <div class="text">Our objective at Bluebell is to bring together our visitor's societies and spirits with our own, communicating enthusiasm <br> and liberality in the food we share. Official Chef and Owner Philippe Massoud superbl.</div>
+            </div>
+            <!--Filter-->
+            <div class="filters style-two">
+                <ul class="filter-tabs filter-btns">
+                    <li class="filter active" data-role="button" data-filter=".all">All</li>
+                    <li class="filter" data-role="button" data-filter=".cat-1">Cozy room</li>
+                    <li class="filter" data-role="button" data-filter=".cat-2">deluxe room</li>
+                    <li class="filter" data-role="button" data-filter=".cat-3">modern room</li>
+                    <li class="filter" data-role="button" data-filter=".cat-4">family suit</li>
+                    <li class="filter" data-role="button" data-filter=".cat-5">grand luxuary room</li>
+                </ul>
+            </div>
+                
+            <!--Sortable Galery-->
+            <div class="sortable-masonry">
+                
+                <div class="items-container row">
+                    <div class="col-lg-6 room-block-two masonry-item all cat-1">
+                        <div class="inner-box">
+                            <div class="image"><img src="assets/images/resource/image-40.jpg" alt="">
+                            <div class="text">$120 / Night</div>
+                            </div>
+                            <h3><a href="booking-reservation.php">Luxuary Room</a> </h3>
+                            <div class="icon-list">
+                                <ul>
+                                    <li><i class="flaticon-bed"></i>2 beds</li>
+                                    <li><i class="flaticon-bath-1"></i>1 Bathroom</li>
+                                    <li><i class="flaticon-blueprint"></i>90 m2</li>
+                                </ul>
+                            </div>
+                            <div class="text-two">Bluebell is to bring together our visitor's societies and spirits with our own, communicating enthusiasm and liberality.</div>
+                            <div class="link-btn"><a href="booking-reservation.php" class="theme-btn btn-style-one btn-md"><span>room details</span></a></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 room-block-two masonry-item all cat-2">
+                        <div class="inner-box">
+                            <div class="image"><img src="assets/images/resource/image-41.jpg" alt="">
+                            <div class="text">$120 / Night</div>
+                            </div>
+                            <h3><a href="booking-reservation.php">Cozy Room</a></h3>
+                            <div class="icon-list">
+                                <ul>
+                                    <li><i class="flaticon-bed"></i>2 beds</li>
+                                    <li><i class="flaticon-bath-1"></i>1 Bathroom</li>
+                                    <li><i class="flaticon-blueprint"></i>90 m2</li>
+                                </ul>
+                            </div>
+                            <div class="text-two">Bluebell is to bring together our visitor's societies and spirits with our own, communicating enthusiasm and liberality.</div>
+                            <div class="link-btn"><a href="booking-reservation.php" class="theme-btn btn-style-one btn-md"><span>room details</span></a></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 room-block-two masonry-item all cat-3">
+                        <div class="inner-box">
+                            <div class="image"><img src="assets/images/resource/image-40.jpg" alt="">
+                            <div class="text">$120 / Night</div>
+                            </div>
+                            <h3><a href="booking-reservation.php">Luxuary Room</a></h3>
+                            <div class="icon-list">
+                                <ul>
+                                    <li><i class="flaticon-bed"></i>2 beds</li>
+                                    <li><i class="flaticon-bath-1"></i>1 Bathroom</li>
+                                    <li><i class="flaticon-blueprint"></i>90 m2</li>
+                                </ul>
+                            </div>
+                            <div class="text-two">Bluebell is to bring together our visitor's societies and spirits with our own, communicating enthusiasm and liberality.</div>
+                            <div class="link-btn"><a href="booking-reservation.php" class="theme-btn btn-style-one btn-md"><span>room details</span></a></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 room-block-two masonry-item all cat-4">
+                        <div class="inner-box">
+                            <div class="image"><img src="assets/images/resource/image-41.jpg" alt="">
+                            <div class="text">$120 / Night</div>
+                            </div>
+                            <h3><a href="booking-reservation.php">Cozy Room</a></h3>
+                            <div class="icon-list">
+                                <ul>
+                                    <li><i class="flaticon-bed"></i>2 beds</li>
+                                    <li><i class="flaticon-bath-1"></i>1 Bathroom</li>
+                                    <li><i class="flaticon-blueprint"></i>90 m2</li>
+                                </ul>
+                            </div>
+                            <div class="text-two">Bluebell is to bring together our visitor's societies and spirits with our own, communicating enthusiasm and liberality.</div>
+                            <div class="link-btn"><a href="booking-reservation.php" class="theme-btn btn-style-one btn-md"><span>room details</span></a></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 room-block-two masonry-item all cat-5">
+                        <div class="inner-box">
+                            <div class="image"><img src="assets/images/resource/image-40.jpg" alt="">
+                            <div class="text">$120 / Night</div>
+                            </div>
+                            <h3><a href="booking-reservation.php">Luxuary Room</a></h3>
+                            <div class="icon-list">
+                                <ul>
+                                    <li><i class="flaticon-bed"></i>2 beds</li>
+                                    <li><i class="flaticon-bath-1"></i>1 Bathroom</li>
+                                    <li><i class="flaticon-blueprint"></i>90 m2</li>
+                                </ul>
+                            </div>
+                            <div class="text-two">Bluebell is to bring together our visitor's societies and spirits with our own, communicating enthusiasm and liberality.</div>
+                            <div class="link-btn"><a href="booking-reservation.php" class="theme-btn btn-style-one btn-md"><span>room details</span></a></div>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-6 room-block-two masonry-item all cat-1">
+                        <div class="inner-box">
+                            <div class="image"><img src="assets/images/resource/image-41.jpg" alt="">
+                            <div class="text">$120 / Night</div>
+                            </div>
+                            <h3><a href="booking-reservation.php">Cozy Room</a></h3>
+                            <div class="icon-list">
+                                <ul>
+                                    <li><i class="flaticon-bed"></i>2 beds</li>
+                                    <li><i class="flaticon-bath-1"></i>1 Bathroom</li>
+                                    <li><i class="flaticon-blueprint"></i>90 m2</li>
+                                </ul>
+                            </div>
+                            <div class="text-two">Bluebell is to bring together our visitor's societies and spirits with our own, communicating enthusiasm and liberality.</div>
+                            <div class="link-btn"><a href="booking-reservation.php" class="theme-btn btn-style-one btn-md"><span>room details</span></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
